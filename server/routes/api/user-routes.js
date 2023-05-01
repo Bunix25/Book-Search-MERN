0df88b2def01084d85f50c795b687a5439fbc420
Bunix25 <116853300+Bunix25@users.noreply.github.com>
2023-05-01 18:13:07 -0400

    Update user-routes.js

--- server/routes/api/user-routes.js
+++ server/routes/api/user-routes.js
@@ -10,3 +10,14 @@ const {
 // import middleware
 const { authMiddleware } = require('../../utils/auth');
 
+router.route('/').post(createUser).put(authMiddleware, saveBook);
+
+router.route('/login').post(login);
+
+router.route('/me').get(authMiddleware, getSingleUser);
+
+router.route('/books/:bookId').delete(authMiddleware, deleteBook);
+
+module.exports = router;
+
+
