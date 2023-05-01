f88b8614992cb26f15133cb1bbd4398f037ee1d3
Bunix25 <116853300+Bunix25@users.noreply.github.com>
2023-05-01 18:04:57 -0400

    Update mutations.js

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,5 +1,6 @@
 import gql from "graphql-tag";
 
+// LOGIN_USER mutation: logs in a user with email and password
 export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
@@ -12,6 +13,7 @@ export const LOGIN_USER = gql`
   }
 `;
 
+// ADD_USER mutation: creates a new user with username, email, and password
 export const ADD_USER = gql`
   mutation addUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
@@ -24,6 +26,7 @@ export const ADD_USER = gql`
   }
 `;
 
+// SAVE_BOOK mutation: adds a book to a user's saved books list
 export const SAVE_BOOK = gql`
   mutation saveBook($book: SavedBookInput!) {
     saveBook(book: $book) {
@@ -42,6 +45,7 @@ export const SAVE_BOOK = gql`
   }
 `;
 
+// REMOVE_BOOK mutation: removes a book from a user's saved books list
 export const REMOVE_BOOK = gql`
   mutation removeBook($bookId: String!) {
     removeBook(bookId: $bookId) {
