0be60f82819a97f2dae41d3ac5f3274346b12ed6
Bunix25 <116853300+Bunix25@users.noreply.github.com>
2023-05-01 18:14:50 -0400

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,11 +2,13 @@ const router = require('express').Router();
 const path = require('path');
 const apiRoutes = require('./api');
 
+// Use the apiRoutes middleware for any routes starting with "/api"
 router.use('/api', apiRoutes);
 
-// serve up react front-end in production
+// Serve the React front-end in production by sending the index.html file in the build folder
 router.use((req, res) => {
   res.sendFile(path.join(__dirname, '../../client/build/index.html'));
 });
 
 module.exports = router;
+
