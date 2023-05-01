328a5d263bfaae0b84378aa3ccf4ef7b6d7e6d07
Bunix25 <116853300+Bunix25@users.noreply.github.com>
2023-05-01 15:49:15 -0400

    Update auth.js

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -9,7 +9,13 @@ class AuthService {
   }
 
  
-
+   // check if user's logged in
+  loggedIn() {
+    // Checks if there is a saved token and it's still valid
+    const token = this.getToken();
+    return !!token && !this.isTokenExpired(token); 
+  }
+  
   // check if token is expired
   isTokenExpired(token) {
     try {
