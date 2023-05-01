e954640ea0e0781e0f5220fb86353762f0b39fd2
Bunix25 <116853300+Bunix25@users.noreply.github.com>
2023-05-01 15:44:58 -0400

    Update SignupForm.js

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,8 +1,12 @@
-
+import React, { useState } from 'react';
+import { Form, Button, Alert } from 'react-bootstrap';
+import Auth from '../utils/auth';
+import { useMutation } from '@apollo/react-hooks';
+import { ADD_USER } from '../utils/mutations';
 
 
 const SignupForm = () => {
-  // set initial form state
+  
   const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
   // set state for form validation
   const [validated] = useState(false);
